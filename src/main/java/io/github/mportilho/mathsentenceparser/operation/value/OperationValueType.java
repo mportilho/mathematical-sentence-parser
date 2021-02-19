9b6ca440d5d92f4a358b8bea8710269182334aef
Marcelo Portilho <marceloport@gmail.com>
2021-02-19 18:33:33 -0300

    operation value type added

---
+++ src/main/java/io/github/mportilho/mathsentenceparser/operation/value/OperationValueType.java
@@ -0,0 +1,7 @@
+package io.github.mportilho.mathsentenceparser.operation.value;
+
+public enum OperationValueType {
+
+	NUMBER, BOOLEAN, STRING, PI, EULER, DATE, TIME, DATETIME, LIST, CURR_DATE, CURR_TIME, CURR_DATETIME, SEQUENCE_VARIABLE, JSON_VARIABLE, VARIABLE; 
+
+}
